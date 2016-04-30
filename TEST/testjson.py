4f45045d93b035e61675e162e5e5d7f43c024e24
ji <jixiaowency@163.com>
2016-04-30 23:20:26 +0800

    0430

--- TEST/testjson.py
+++ TEST/testjson.py
@@ -6,4 +6,23 @@ from pprint import pprint
 with open("./schedule_2016_03.json", encoding='utf8') as f:
     data = json.load(f)
 
-pprint(data)
+
+want_data = data["payload"]["dates"][0]["games"][0]
+
+def get_team_data(team_type):
+
+    matchup = team_type["matchup"]
+    confRank = matchup["confRank"]
+    divRank = matchup["divRank"]
+    losses = matchup["losses"]
+
+    profile = team_type["profile"]
+    abbr = profile["abbr"]
+    city = profile["city"]
+    cityEn = profile["cityEn"]
+    code = profile["code"]
+    conference = profile["conference"]
+
+
+
+
