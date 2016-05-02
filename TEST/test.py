e311109f4dac5c8fba0a160902e926cabcf5dcdd
ji <jixiaowency@163.com>
2016-05-02 15:40:57 +0800

    0430

--- TEST/test.py
+++ TEST/test.py
@@ -1,1 +1,25 @@
-i
+#!/usr/bin/env python3
+# --*-- coding: utf-8 --*--
+
+import os
+import sqlite3
+
+
+i = os.listdir("../session_data/")
+# for l in i:
+#     print(l)
+
+
+con = sqlite3.connect("/home/ji/pythonScripts/NBA/tmp.db")
+
+cl = con.cursor()
+
+cl.execute("create table if not exists tmp(filename text, isdone text) ")
+
+
+for filename in i:
+    cl.execute("insert into tmp values (?, ?)", (filename, 'N'))
+
+con.commit()
+
+
